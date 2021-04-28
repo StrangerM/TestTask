af2ebb8b2e92b3fc3b3a2baa890d3da13b336455
Yuriy Plyska <yuriyply@mail.com>
2021-04-28 22:22:40 +0300

    code was a little bit optimized

--- Program.cs
+++ Program.cs
@@ -10,18 +10,14 @@ namespace TestTask
         {
             IEnumerable<int> result = new List<int>();
 
-            result = CheckNumSum(-170, 170, 8);
+            result = CheckNumSum(-170, 170, 0);
 
             if(result.Count() == 0) 
-            {
                 Console.WriteLine("There is no acceptable number according to the conditions of the task ");
-            }
             else 
             {
                foreach(var item in result) 
-               {
                  Console.WriteLine("There is acceptable number according to the conditions of the task- {0}", item);
-               }
             }
             Console.ReadLine();
         }
@@ -35,27 +31,20 @@ namespace TestTask
                 var temp = x.ToString().Select(x => x).ToList();
 
                 if (x < 0)
-                {
-                    
+                {  
                     temp.RemoveAt(0);
                     var intList = temp.Select(digit => int.Parse(digit.ToString())).ToList();
                     intList[0] *= -1;
                     var resultOfSum = intList.Sum(x => x);
                     if (resultOfSum == sum)
-                    {
-                        result.Add(x);
-                    }
+                          result.Add(x);
                 }
                 else
                 {
-                    var intList = temp.Select(digit => int.Parse(digit.ToString()));
-                    var resultOfSum = intList.Sum(x => x);
-                    if (resultOfSum == sum)
-                    {
-                        result.Add(x);
-                    }
+                    var intList = temp.Select(digit => int.Parse(digit.ToString())).Sum(x=>x);    
+                    if (intList == sum)
+                          result.Add(x);
                 }
-
             }
             return result;
         }
